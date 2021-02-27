ff164a868684e15c0efe8b6ae098e5975e149f82
Marco Gomiero <marco.gomiero.93@gmail.com>
2021-02-27 13:59:56 +0100

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    kotlin("multiplatform") version "1.4.21"
+    id("com.android.library")
+//    id("com.prof18.kmp.fatframework.cocoa") version "0.0.1-SNAPSHOT"
+    id("com.prof18.kmp.fatframework.cocoa") version "0.0.1"
+}
+
+group = "com.prof18"
+version = "1.0.1-SNAPSHOT"
+
+repositories {
+//    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+
+
+
+kotlin {
+    android()
+    ios {
+        binaries.framework("LibraryName")
+    }
+//    macosX64("macOS") {
+//        binaries.framework("LibraryName")
+//    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.2.1")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13")
+            }
+        }
+//        val iosMain by getting
+//        val iosTest by getting
+
+//        val macOSMain by getting
+//        val macOSTest by getting
+    }
+}
+
+android {
+    compileSdkVersion(29)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(29)
+    }
+}
+
+fatFrameworkCocoaConfig {
+    fatFrameworkName = "LibraryName"
+    outputPath = "$rootDir/../test-dest"
+    namePrefix = "LibraryName"
+    versionName = "1.2-SNAPSHOT"
+
+    cocoaPodRepoInfo {
+        summary = "This is a test KMP framework"
+        homepage = "https://github.com/prof18/ccoca-repo-test"
+        license = "Apache"
+        authors = "\"Marco Gomiero\" => \"mg@me.com\""
+        gitUrl = "git@github.com:prof18/ccoca-repo-test.git"
+    }
+
+}
---
+++ src/commonMain/kotlin/com/prof18/library/Greeting.kt
@@ -0,0 +1,8 @@
+package com.prof18.library
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
